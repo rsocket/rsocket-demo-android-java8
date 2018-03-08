ab073f303ad6b1976c602a208f9c38aee33b35dd
Yuri Schimke <yuri@schimke.ee>
2018-03-08 05:55:32 +0000

    version bumps

--- app/src/main/java/io/rsocket/rsocketdemoappjava8/MainActivity.kt
+++ app/src/main/java/io/rsocket/rsocketdemoappjava8/MainActivity.kt
@@ -9,9 +9,9 @@ import android.view.Menu
 import android.view.MenuItem
 import io.rsocket.RSocketFactory
 import io.rsocket.transport.netty.client.WebsocketClientTransport
-import io.rsocket.util.PayloadImpl
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import io.rsocket.util.DefaultPayload
+import kotlinx.android.synthetic.main.activity_main.toolbar
+import kotlinx.android.synthetic.main.content_main.label
 import reactor.core.Disposable
 import reactor.core.scheduler.Schedulers
 import java.net.URI
@@ -19,43 +19,43 @@ import java.time.Duration.ofSeconds
 
 class MainActivity : AppCompatActivity() {
 
-    private var subscription: Disposable? = null
+  private var subscription: Disposable? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    setSupportActionBar(toolbar)
 
-        val mUiHandler = object : Handler(Looper.getMainLooper()) {}
+    val mUiHandler = object : Handler(Looper.getMainLooper()) {}
 
-        val ws = WebsocketClientTransport.create(URI.create("ws://rsocket-demo.herokuapp.com/ws"));
-        val client = RSocketFactory.connect().keepAlive().transport(ws).start()
+    val ws = WebsocketClientTransport.create(URI.create("ws://rsocket-demo.herokuapp.com/ws"));
+    val client = RSocketFactory.connect().keepAlive().transport(ws).start()
 
-        val trumpTweets = client.flatMapMany { it.requestStream(PayloadImpl("trump")) }
-        val onePerSec = trumpTweets.window(ofSeconds(1L)).flatMap { it.take(1L) }
-        subscription = onePerSec.subscribeOn(Schedulers.elastic()).subscribe(
-                { mUiHandler.post({ label.text = it.dataUtf8 }) },
-                { Log.w("MainActivity", "failed", it) })
+    val trumpTweets = client.flatMapMany { it.requestStream(DefaultPayload.create("trump")) }
+    val onePerSec = trumpTweets.window(ofSeconds(1L)).flatMap { it.take(1L) }
+    subscription = onePerSec.subscribeOn(Schedulers.elastic()).subscribe(
+        { mUiHandler.post({ label.text = it.dataUtf8 }) },
+        { Log.w("MainActivity", "failed", it) })
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    menuInflater.inflate(R.menu.menu_main, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    return when (item.itemId) {
+      R.id.action_settings -> true
+      else -> super.onOptionsItemSelected(item)
     }
+  }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onDestroy() {
-        subscription?.dispose()
-        super.onDestroy()
-    }
+  override fun onDestroy() {
+    subscription?.dispose()
+    super.onDestroy()
+  }
 }
